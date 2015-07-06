44aa886b81270c4aaff51f528981a52d1bb05d76
Samuel ROZE <samuel.roze@gmail.com>
2015-07-06 23:15:56 +0100

    Display running container with their addresses

---
+++ src/Cli/PsCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Dock\Cli;
+
+use Dock\Cli\IO\ConsoleUserInteraction;
+use Dock\Compose\Container;
+use Dock\Compose\Inspector;
+use Dock\Installer\InteractiveProcessRunner;
+use Dock\IO\ProcessRunner;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableSeparator;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class PsCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('ps')
+            ->setDescription('List running containers')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userInteraction = new ConsoleUserInteraction($input, $output);
+        $processRunner = new InteractiveProcessRunner($userInteraction);
+
+        $inspector = new Inspector($processRunner);
+        $containers = $inspector->getRunningContainers();
+
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'DNS addresses', 'Port(s)', 'Status']);
+
+        foreach ($containers as $index => $container) {
+            if ($index > 0) {
+                $table->addRow(new TableSeparator());
+            }
+
+            $table->addRow([
+                $container->getName(),
+                implode("\n", $container->getHosts()),
+                implode("\n", $container->getPorts()),
+                $this->getDecoratedState($container)
+            ]);
+        }
+
+        $table->render();
+    }
+
+    /**
+     * @param Container $container
+     * @return string
+     */
+    private function getDecoratedState(Container $container)
+    {
+        if ($container->getState() === Container::STATE_EXITED) {
+            return '<error>'.$container->getState().'</error>';
+        }
+
+        return $container->getState();
+    }
+}
---
+++ src/Compose/Container.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Dock\Compose;
+
+class Container
+{
+    const STATE_UNKNOWN = 'unknown';
+    const STATE_RUNNING = 'running';
+    const STATE_EXITED = 'exited';
+
+    /**
+     * @var string
+     */
+    private $name;
+    /**
+     * @var string
+     */
+    private $image;
+    /**
+     * @var array
+     */
+    private $hosts;
+    /**
+     * @var string
+     */
+    private $state;
+    /**
+     * @var array
+     */
+    private $ports;
+
+    /**
+     * @param string $name
+     * @param string $image
+     * @param string $state
+     * @param array $hosts
+     * @param array $ports
+     */
+    public function __construct($name, $image, $state = self::STATE_UNKNOWN, array $hosts = [], array $ports = [])
+    {
+        $this->name = $name;
+        $this->image = $image;
+        $this->hosts = $hosts;
+        $this->state = $state;
+        $this->ports = $ports;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @return array
+     */
+    public function getHosts()
+    {
+        return $this->hosts;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPorts()
+    {
+        return $this->ports;
+    }
+}
