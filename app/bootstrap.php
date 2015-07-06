31168bc626cef30b4464bf587e01b8c3499af98f
Tony Piper <tony.piper@antiphonal.net>
2015-07-06 22:29:22 +0100

    configure commands using the container, remove redundant application class

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,49 @@
 <?php
 
 use Pimple\Container;
-use Dock\Cli\Application;
+use Symfony\Component\Console\Application;
 
 $container = new Container();
 
+$container['command.help'] = function ($c) {
+    return new \Symfony\Component\Console\Command\HelpCommand();
+};
+
+$container['command.list'] = function ($c) {
+    return new \Symfony\Component\Console\Command\ListCommand();
+};
+
+$container['command.selfupdate'] = function ($c) {
+    return new \Dock\Cli\SelfUpdateCommand();
+};
+
+$container['command.install'] = function ($c) {
+    return new \Dock\Cli\InstallCommand();
+};
+
+$container['command.restart'] = function ($c) {
+    return new \Dock\Cli\RestartCommand();
+};
+
+$container['command.up'] = function ($c) {
+    return new \Dock\Cli\UpCommand();
+};
+
+
 $container['application'] = function ($c) {
-    return new Application('Dock CLI', '@package_version@');
+    $application = new Application('Dock CLI', '@package_version@');
+    $application->addCommands(
+        array(
+            $c['command.help'],
+            $c['command.list'],
+            $c['command.selfupdate'],
+            $c['command.install'],
+            $c['command.restart'],
+            $c['command.up']
+        )
+    );
+
+    return $application;
 };
 
 return $container;
