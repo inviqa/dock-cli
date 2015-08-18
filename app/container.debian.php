8ba32997762a3b91030d1f259825c7c1b08ff3b6
Marek Matulka <marek@matulka.net>
2015-08-18 17:08:40 +0100

    Refactor duplication

--- app/container.debian.php
+++ app/container.debian.php
@@ -1,36 +1,9 @@
 <?php
 
-use Dock\Doctor;
 use Dock\Installer\DNS;
-use Dock\Installer\Docker;
-use Dock\Installer\System;
-use Dock\Installer\TaskProvider;
-use Dock\System\Linux\ShellCreator;
 
-$container['system.shell_creator'] = function() {
-    return new ShellCreator();
-};
+require dirname(__FILE__) . DIRECTORY_SEPARATOR . 'container.linux.php';
 
-$container['installer.dns.dnsdock'] = function($c) {
-    return new Dns\Linux\DnsDock($c['console.user_interaction'], $c['process.interactive_runner']);
-};
 $container['installer.dns.docker_routing'] = function($c) {
-    return new Dns\Linux\Debian\DockerRouting($c['console.user_interaction'], $c['process.interactive_runner']);
-};
-
-$container['installer.task_provider'] = function ($c) {
-    return new TaskProvider([
-        new System\Linux\NoSudo($c['console.user_interaction'], $c['process.interactive_runner']),
-        new System\Linux\Docker($c['console.user_interaction'], $c['process.interactive_runner']),
-        new System\Linux\DockerCompose($c['console.user_interaction'], $c['process.interactive_runner']),
-        $c['installer.dns.dnsdock'],
-        $c['installer.dns.docker_routing'],
-    ]);
-};
-
-$container['doctor.tasks'] = function($c) {
-    return [
-        new Doctor\Docker($c['process.silent_runner'], $c['installer.docker']),
-        new Doctor\DnsDock($c['process.silent_runner'], $c['installer.dns.dnsdock'], $c['installer.dns.docker_routing']),
-    ];
+    return new DNS\Linux\Debian\DockerRouting($c['console.user_interaction'], $c['process.interactive_runner']);
 };
---
+++ app/container.linux.php
@@ -0,0 +1,33 @@
+<?php
+
+use Dock\Doctor;
+use Dock\Installer\DNS;
+use Dock\Installer\Docker;
+use Dock\Installer\System;
+use Dock\Installer\TaskProvider;
+use Dock\System\Linux\ShellCreator;
+
+$container['system.shell_creator'] = function() {
+    return new ShellCreator();
+};
+
+$container['installer.dns.dnsdock'] = function($c) {
+    return new Dns\Linux\DnsDock($c['console.user_interaction'], $c['process.interactive_runner']);
+};
+
+$container['installer.task_provider'] = function ($c) {
+    return new TaskProvider([
+        new System\Linux\NoSudo($c['console.user_interaction'], $c['process.interactive_runner']),
+        new System\Linux\Docker($c['console.user_interaction'], $c['process.interactive_runner']),
+        new System\Linux\DockerCompose($c['console.user_interaction'], $c['process.interactive_runner']),
+        $c['installer.dns.dnsdock'],
+        $c['installer.dns.docker_routing'],
+    ]);
+};
+
+$container['doctor.tasks'] = function($c) {
+    return [
+        new Doctor\Docker($c['process.silent_runner'], $c['installer.docker']),
+        new Doctor\DnsDock($c['process.silent_runner'], $c['installer.dns.dnsdock'], $c['installer.dns.docker_routing']),
+    ];
+};
