21bf2a428892c8baf996be93aafb25a2121f04e9
Marek Matulka <marek@matulka.net>
2015-08-18 16:55:13 +0100

    Enable support for Fedora (RedHat/CentOs should work)

---
+++ app/container.redhat.php
@@ -0,0 +1,36 @@
+<?php
+
+use Dock\Doctor;
+use Dock\Installer\DNS;
+use Dock\Installer\Docker;
+use Dock\Installer\System;
+use Dock\Installer\TaskProvider;
+use Dock\System\Linux\ShellCreator;
+
+$container['system.shell_creator'] = function() {
+    return new ShellCreator();
+};
+
+$container['installer.dns.dnsdock'] = function($c) {
+    return new Dns\Linux\DnsDock($c['console.user_interaction'], $c['process.interactive_runner']);
+};
+$container['installer.dns.docker_routing'] = function($c) {
+    return new Dns\Linux\RedHat\DockerRouting($c['console.user_interaction'], $c['process.interactive_runner']);
+};
+
+$container['installer.task_provider'] = function ($c) {
+    return new TaskProvider([
+        new System\Linux\RedHat\NoSudo($c['console.user_interaction'], $c['process.interactive_runner']),
+        new System\Linux\Docker($c['console.user_interaction'], $c['process.interactive_runner']),
+        new System\Linux\DockerCompose($c['console.user_interaction'], $c['process.interactive_runner']),
+        $c['installer.dns.dnsdock'],
+        $c['installer.dns.docker_routing'],
+    ]);
+};
+
+$container['doctor.tasks'] = function($c) {
+    return [
+        new Doctor\Docker($c['process.silent_runner'], $c['installer.docker']),
+        new Doctor\DnsDock($c['process.silent_runner'], $c['installer.dns.dnsdock'], $c['installer.dns.docker_routing']),
+    ];
+};
---
+++ src/Installer/DNS/Linux/RedHat/DockerRouting.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Dock\Installer\DNS\Linux\RedHat;
+
+use Dock\Installer\DNS\Linux\DnsDock;
+use Dock\Installer\InstallerTask;
+use Dock\IO\ProcessRunner;
+use Dock\IO\UserInteraction;
+
+class DockerRouting extends InstallerTask
+{
+    /**
+     * @var ProcessRunner
+     */
+    private $processRunner;
+    /**
+     * @var UserInteraction
+     */
+    private $userInteraction;
+
+    /**
+     * @param UserInteraction $userInteraction
+     * @param ProcessRunner $processRunner
+     */
+    public function __construct(UserInteraction $userInteraction, ProcessRunner $processRunner)
+    {
+        $this->userInteraction = $userInteraction;
+        $this->processRunner = $processRunner;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'routing';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        if (! $this->isUsingDnsDockDnsServer()) {
+            $this->userInteraction->writeTitle('Configure routing for direct Docker containers access');
+
+            //$this->processRunner->run('echo "nameserver ' . DnsDock::IP . '" | sudo tee -a /etc/resolv.conf');
+            $this->processRunner->run("sudo sed -i -e '1inameserver " . DnsDock::IP . "\\' /etc/resolv.conf");
+        }
+    }
+
+    private function isUsingDnsDockDnsServer()
+    {
+        return $this->processRunner->run('grep "' . DnsDock::IP . '" /etc/resolv.conf', false)->isSuccessful();
+    }
+}
