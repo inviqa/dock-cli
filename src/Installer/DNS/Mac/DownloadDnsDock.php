798a158e3b432c48024beddf49f58338865ba672
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-02-23 12:24:11 +0000

    Pull DnsDock image after installing to improve install experience for slow internet connections.

---
+++ src/Installer/DNS/Mac/DownloadDnsDock.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Dock\Installer\DNS\Mac;
+
+use Dock\Installer\InstallerTask;
+use Dock\IO\ProcessRunner;
+use Dock\IO\UserInteraction;
+use SRIO\ChainOfResponsibility\DependentChainProcessInterface;
+
+class DownloadDnsDock extends InstallerTask implements DependentChainProcessInterface
+{
+    /**
+     * @var UserInteraction
+     */
+    private $userInteraction;
+
+    /**
+     * @var ProcessRunner
+     */
+    private $processRunner;
+
+    /**
+     * @param UserInteraction $userInteraction
+     * @param ProcessRunner $processRunner
+     */
+    public function __construct(UserInteraction $userInteraction, ProcessRunner $processRunner)
+    {
+        $this->userInteraction = $userInteraction;
+        $this->processRunner = $processRunner;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function run()
+    {
+        $this->userInteraction->writeTitle("Pulling image tonistiigi/dnsdock");
+        $this->userInteraction->write("This could take a while when run for the first time");
+        $this->processRunner->run('docker pull tonistiigi/dnsdock');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function dependsOn()
+    {
+        return ['machine'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'dnsdock';
+    }
+}
